f4e7ce5e8ab6dd8d22b8dbf000a1aa433c280234
Young Ju Kim <pydemia@gmail.com>
2018-06-26 21:11:21 +0900

    mac layer

--- qmk/keyboards/ergodox/ez/keymaps/pydemia/keymap.c
+++ qmk/keyboards/ergodox/ez/keymaps/pydemia/keymap.c
@@ -726,9 +726,9 @@ KC_DOWN,     KC_DELT,  KC_ENT
 KC_ESC,           KC_1,           KC_2,           KC_3,         KC_4,           KC_5,       LSFT(KC_ENT), // TG(LKEY),
 KC_TAB,           KC_Q,           KC_W,           KC_E,         KC_R,           KC_T,       ASSIGN_TAP,
 KC_GRV,           KC_A,           KC_S,           KC_D,         KC_F,           KC_G,
-KC_LSFT,          KC_Z,           KC_X,           KC_C,         KC_V,           KC_B,       KC_TRNS,
+KC_LSFT,          KC_Z,           KC_X,           KC_C,         KC_V,           KC_B,       KC_FN1,
 CTL_T(KC_LCTL),   KC_TRNS,        KC_LGUI,        KC_APP,       ALT_T(KC_LALT),
-                                                                        KC_TRNS,          LCTL(KC_ENT),
+                                                                        KC_FN3,           LCTL(KC_ENT),
                                                                                           KC_HOME,
                                                       KC_SPC,           KC_BSPC,          KC_END,
 // right hand
